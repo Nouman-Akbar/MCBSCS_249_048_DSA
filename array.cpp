4769277701b4e4840227eb6ad8d8d1eb8f4d3964
Muhammad Nouman Akbar <noumanakbardeveloper@gmail.com>
2025-11-12 10:20:57 +0500

    added the files

---
+++ array.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio>
+using namespace std;
+int main()
+{
+    int arr[5] = [0,1,2,3,4];
+    for(int i=0; i<5; i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+    return 0;
+}
+
+// compiler -- Interpreter
+// dynamic and static type casting
+// print function for numerical values and string values
---
+++ array.py
@@ -0,0 +1,13 @@
+array = [1, 2, 3, 4, 5]
+print(array)
+array.append(6)
+print(array)
+
+
+x = ['hello World', 12, 3.4, True, {'name': 'Fahad'}]
+
+print(x)
+print(x[0])
+
+arrayList = list(range(1, 11))
+print(arrayList)
